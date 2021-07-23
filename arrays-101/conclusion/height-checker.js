0d09070b10b3eec74cdacb99a8165c3f2489016f
Liam Sutton <devliam90@gmail.com>
2021-07-23 15:14:01 +0100

    fix height checker sort logic

--- arrays-101/conclusion/height-checker.js
+++ arrays-101/conclusion/height-checker.js
@@ -4,7 +4,7 @@ function heightChecker(heights) {
   // place to store the result (number of moves)
   let moves = 0;
   // place to store a sorted array
-  const sorted = [...heights].sort();
+  const sorted = [...heights].sort((a, b) => a - b);
   // iterate over the heights
   for (let i = 0; i < heights.length; i++) {
     // if element at index in heights is not equal to element at index in sorted
@@ -19,7 +19,7 @@ function heightChecker(heights) {
 
 function heightChecker(heights) {
   let moves = 0;
-  const sorted = [...heights].sort();
+  const sorted = [...heights].sort((a, b) => a - b);
 
   heights.map((num, i) => {
     if (num != sorted[i]) {
@@ -31,7 +31,7 @@ function heightChecker(heights) {
 }
 
 function heightChecker(heights) {
-  const sorted = [...heights].sort();
+  const sorted = [...heights].sort((a, b) => a - b);
 
   return heights.reduce((total, _, i) => {
     if (sorted[i] != heights[i]) {
@@ -42,7 +42,7 @@ function heightChecker(heights) {
 }
 
 function heightChecker(heights) {
-  const sorted = [...heights].sort();
+  const sorted = [...heights].sort((a, b) => a - b);
 
   return heights.reduce(
     (total, _, i) => (heights[i] != sorted[i] ? total + 1 : total),
@@ -50,6 +50,12 @@ function heightChecker(heights) {
   );
 }
 
+console.log(
+  heightChecker([
+    10, 6, 6, 10, 10, 9, 8, 8, 3, 3, 8, 2, 1, 5, 1, 9, 5, 2, 7, 4, 7, 7,
+  ]),
+  22
+);
 console.log(heightChecker([1, 1, 4, 2, 1, 3]), 3);
 console.log(heightChecker([5, 1, 2, 3, 4]), 5);
 console.log(heightChecker([1, 2, 3, 4, 5]), 0);
